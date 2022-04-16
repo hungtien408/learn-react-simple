a3fac98277117f934dbd64a27f55e7eb67c8fd4f
tien.nh <hungtien408@gmail.com>
2022-04-16 23:29:52 +0700

    Làm giao diên thanh header

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
+import Header from 'components/Header';
 import { useEffect } from 'react';
-import { Link, NavLink, Redirect, Route, Switch } from 'react-router-dom';
+import { Redirect, Route, Switch } from 'react-router-dom';
 import productApi from './api/productApi';
 import './App.css';
 import NotFound from './components/NotFound';
 import AlbumFeature from './features/Album';
 import CounterFeature from './features/Counter';
 import TodoFeature from './features/Todo';
-import styled from 'styled-components';
-
-// CSS in JS
-const Title = styled.h1`
-  text-align: center;
-  font-weight: bold;
-
-  color: ${(props) => props.color || 'green'};
-`;
 
 function App() {
   useEffect(() => {
@@ -31,26 +23,7 @@ function App() {
 
   return (
     <div className="App">
-      {/* <AlbumFeature />
-      <ColorBox />
-      <Counter /> */}
-      <Title color="goldenrod">HEADING</Title>
-
-      <p>
-        <Link to="/todos">Todos</Link> <Link to="/albums">Albums</Link>
-      </p>
-
-      <p>
-        {/*
-          Link not have class active 
-          NavLink default have class active, can set class active to other class 
-        */}
-        <NavLink to="/todos" activeClassName="active-menu">
-          Todos
-        </NavLink>{' '}
-        <NavLink to="/albums">Albums</NavLink>
-      </p>
-
+      <Header />
       {/*
           Switch chỉ render route match lần đầu, tránh render duplicate 
         */}
