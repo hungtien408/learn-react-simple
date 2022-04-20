5023aa25458eba6f1d404d85e8c844513acbe88e
tien.nh <hungtien408@gmail.com>
2022-04-20 22:05:28 +0700

    Style cho form đăng ký

--- src/features/Auth/components/RegisterForm/index.jsx
+++ src/features/Auth/components/RegisterForm/index.jsx
@@ -1,11 +1,28 @@
 import { yupResolver } from '@hookform/resolvers/yup';
-import { Avatar, Typography } from '@material-ui/core';
+import { Avatar, Button, makeStyles, Typography } from '@material-ui/core';
 import { LockOutlined } from '@material-ui/icons';
 import InputField from 'components/form-controls/InputField';
 import PropTypes from 'prop-types';
 import React from 'react';
 import { useForm } from 'react-hook-form';
 import * as yup from 'yup';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    paddingTop: theme.spacing(4),
+  },
+  avatar: {
+    margin: '0 auto',
+    backgroundColor: theme.palette.secondary.main,
+  },
+  title: {
+    margin: theme.spacing(2, 0, 3),
+    textAlign: 'center',
+  },
+  submit: {
+    margin: theme.spacing(2, 0),
+  },
+}));
 
 RegisterFrom.propTypes = {
   onSubmit: PropTypes.func,
@@ -16,6 +33,7 @@ RegisterFrom.defaultProps = {
 };
 
 function RegisterFrom(props) {
+  const classes = useStyles();
   const { onSubmit } = props;
 
   const schema = yup.object().shape({
@@ -40,12 +58,12 @@ function RegisterFrom(props) {
   };
 
   return (
-    <div>
-      <Avatar>
+    <div className={classes.root}>
+      <Avatar className={classes.avatar}>
         <LockOutlined />
       </Avatar>
 
-      <Typography component="h1" variant="h5">
+      <Typography className={classes.title} component="h1" variant="h5">
         Create An Account
       </Typography>
 
@@ -54,6 +72,10 @@ function RegisterFrom(props) {
         <InputField name="email" label="Email" form={form} />
         <InputField name="password" label="Password" form={form} />
         <InputField name="retypePassword" label="Retype Password" form={form} />
+
+        <Button className={classes.submit} variant="contained" color="primary" fullWidth>
+          Create An Account
+        </Button>
       </form>
     </div>
   );
