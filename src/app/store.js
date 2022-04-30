34cc31d915d4819a23ec54b8a4fe71746663b230
tien.nh <hungtien408@gmail.com>
2022-04-30 21:20:08 +0700

    Setup userSlice

--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,10 @@
 import { configureStore } from '@reduxjs/toolkit';
+import userReducer from '../features/Auth/userSlice';
 import counterReducer from '../features/Counter/counterSlice';
 
 const rootReducer = {
   counter: counterReducer,
+  user: userReducer,
 };
 
 const store = configureStore({
