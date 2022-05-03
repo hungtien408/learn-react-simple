1a68da196efa12024eb0c0c34463d19236b72124
tien.nh <hungtien408@gmail.com>
2022-05-03 20:45:05 +0700

    Setup API đăng nhập

--- src/features/Auth/userSlice.js
+++ src/features/Auth/userSlice.js
@@ -12,6 +12,17 @@ export const register = createAsyncThunk('users/register', async (payload) => {
   return data.user;
 });
 
+export const login = createAsyncThunk('users/login', async (payload) => {
+  // call api to register
+  const data = await userApi.login(payload);
+
+  // save data to local storage
+  localStorage.setItem('access_token', data.jwt);
+  localStorage.setItem('user', JSON.stringify(data.user));
+
+  return data.user;
+});
+
 const userSlice = createSlice({
   name: 'user',
   initialState: {
@@ -21,6 +32,10 @@ const userSlice = createSlice({
   reducers: {},
   extraReducers: {
     [register.fulfilled]: (state, action) => {
+      state.current = action.payload;
+    },
+
+    [login.fulfilled]: (state, action) => {
       state.current = action.payload;
     },
   },
