6300ad6d1ef16583b74b44097ab2932b4d2fc764
tien.nh <hungtien408@gmail.com>
2022-05-25 22:12:28 +0700

    Format giá sản phẩm theo đơn vị VND

--- src/features/Product/components/ProductItem/index.jsx
+++ src/features/Product/components/ProductItem/index.jsx
@@ -23,7 +23,10 @@ function ProductItem({ product }) {
       </Box>
       <Typography variant="body2">{product.name}</Typography>
       <Typography variant="body2">
-        {product.salePrice} -{product.promotionPercent}
+        <Box component={'span'} fontSize="16px" fontWeight="bold" mr={1}>
+          {new Intl.NumberFormat('vi-VN', { style: 'currency', currency: 'VND' }).format(product.salePrice)}
+        </Box>
+        {product.promotionPercent > 0 ? ` -${product.promotionPercent}%` : ''}
       </Typography>
     </Box>
   );
