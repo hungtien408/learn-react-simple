9e32dbbda0ce5738ad8440061ff2bc48836a414d
tien.nh <hungtien408@gmail.com>
2022-06-14 23:36:07 +0700

    134. Lọc theo khoảng giá

---
+++ src/features/Product/components/ProductFilter/Filters/FilterByPrice/index.jsx
@@ -0,0 +1,43 @@
+import { Box, Button, TextField, Typography } from '@material-ui/core';
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
+
+FilterByPrice.propTypes = {
+  onChange: PropTypes.func,
+};
+
+function FilterByPrice({ onChange }) {
+  const [values, setValues] = useState({
+    salePrice_gte: 0,
+    salePrice_lte: 0,
+  });
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setValues((prevValues) => ({
+      ...prevValues,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = () => {
+    if (!onChange) return;
+    onChange(values);
+  };
+
+  return (
+    <Box>
+      <Typography variant="subtitle2">GIÁ</Typography>
+      <Box>
+        <TextField name="salePrice_gte" value={values.salePrice_gte} onChange={handleChange} />
+        <span>-</span>
+        <TextField name="salePrice_lte" value={values.salePrice_lte} onChange={handleChange} />
+      </Box>
+      <Button variant="outlined" color="primary" onClick={handleSubmit}>
+        Áp dụng
+      </Button>
+    </Box>
+  );
+}
+
+export default FilterByPrice;
--- src/features/Product/components/ProductFilter/index.jsx
+++ src/features/Product/components/ProductFilter/index.jsx
@@ -2,6 +2,7 @@ import { Box } from '@material-ui/core';
 import PropTypes from 'prop-types';
 import React from 'react';
 import FilterByCategory from './Filters/FilterByCategory';
+import FilterByPrice from './Filters/FilterByPrice';
 
 ProductFilter.propTypes = {
   filters: PropTypes.object.isRequired,
@@ -13,15 +14,20 @@ function ProductFilter({ filters, onChange }) {
     if (!onChange) return;
 
     const newFilters = {
-      ...filters,
       "category.id": newCategoryId,
     };
     onChange(newFilters);
   };
 
+  const handlePriceChange = (values) => {
+    if (!onChange) return;
+    onChange(values);
+  }
+
   return (
     <Box>
       <FilterByCategory onChange={handlerCategoryChange} />
+      <FilterByPrice onChange={handlePriceChange} />
     </Box>
   );
 }
