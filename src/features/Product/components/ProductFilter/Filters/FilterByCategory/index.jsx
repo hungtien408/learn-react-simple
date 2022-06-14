e091b8990c8c751920b624ee86ee32c7dc9be132
tien.nh <hungtien408@gmail.com>
2022-06-14 22:45:22 +0700

    133. Giao diện lọc danh mục sản phẩm

--- src/features/Product/components/ProductFilter/Filters/FilterByCategory/index.jsx
+++ src/features/Product/components/ProductFilter/Filters/FilterByCategory/index.jsx
@@ -1,7 +1,28 @@
-import { Box, Typography } from '@material-ui/core';
+import { Box, makeStyles, Typography } from '@material-ui/core';
+import { Skeleton } from '@material-ui/lab';
 import categoryApi from 'api/categoryApi';
 import PropTypes from 'prop-types';
 import React, { useEffect, useState } from 'react';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+  menu: {
+    padding: 0,
+    margin: 0,
+    listStyleType: 'none',
+    '& > li': {
+      marginTop: theme.spacing(1),
+      transition: 'all .25s',
+      '&:hover': {
+        cursor: 'pointer',
+        color: theme.palette.primary.dark,
+        fontWeight: 'bold',
+      },
+    },
+  },
+}));
 
 FilterByCategory.propTypes = {
   onChange: PropTypes.func,
@@ -9,6 +30,8 @@ FilterByCategory.propTypes = {
 
 function FilterByCategory({ onChange }) {
   const [categoryList, setCategoryList] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const classes = useStyles();
 
   useEffect(() => {
     const fetchCategories = async () => {
@@ -23,6 +46,8 @@ function FilterByCategory({ onChange }) {
       } catch (error) {
         console.log('Failed to fetch category list: ', error);
       }
+
+      setLoading(false);
     };
     fetchCategories();
   }, []);
@@ -33,15 +58,21 @@ function FilterByCategory({ onChange }) {
   };
 
   return (
-    <Box>
-      <Typography>DANH MỤC SẢN PHẨM</Typography>
+    <Box className={classes.root}>
+      <Typography variant="subtitle2">DANH MỤC SẢN PHẨM</Typography>
 
-      <ul>
-        {categoryList.map((category) => (
-          <li key={category.id} onClick={() => handleCategoryClick(category)}>
-            {category.name}
-          </li>
-        ))}
+      <ul className={classes.menu}>
+        {loading
+          ? Array.from(new Array(8)).map((x, index) => (
+              <li key={index}>
+                <Skeleton variant="rect" width="100%" />
+              </li>
+            ))
+          : categoryList.map((category) => (
+              <li key={category.id} onClick={() => handleCategoryClick(category)}>
+                <Typography variant="body2">{category.name}</Typography>
+              </li>
+            ))}
       </ul>
     </Box>
   );
